d0d5ec1bcdad7dffc6da671265c5b0484fbeb111
Abhikr0 <abhishekkumar.ak2210@gmail.com>
2024-04-23 10:49:45 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <button className="relative text-white cursor-pointer py-2 px-4 border border-transparent rounded-lg bg-black font-semibold text-xl tracking-widest focus:outline-none focus:ring-4 focus:ring-glow-color focus:ring-opacity-50">
+      <span className="float-left mr-3 text-shadow-flicker">C</span>
+      <span className="text-shadow-flicker">L</span>
+      <span className="text-shadow-flicker">I</span>
+      <span className="text-shadow-flicker">C</span>
+      <span className="text-shadow-flicker">K</span>
+    </button>
+    </>
+  )
+}
+
+export default App
